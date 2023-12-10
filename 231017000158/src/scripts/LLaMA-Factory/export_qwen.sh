c20691127d102805432d2f329bfd698d1d1b8172
monicahkk <guyuehukuku@gmail.com>
2023-12-10 20:22:32 +0800

    description of edit

--- 231017000158/src/scripts/LLaMA-Factory/export_qwen.sh
+++
@@ -1,6 +0,0 @@
-python src/export_model.py \
-    --model_name_or_path /home/ubuntu/huggingface/Qwen-14B-Chat \
-    --template chatml \
-    --finetuning_type lora \
-    --checkpoint_dir Qwen_for_match \
-    --export_dir Qwen_for_match_merged
