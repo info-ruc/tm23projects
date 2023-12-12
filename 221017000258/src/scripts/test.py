6f0f8ad7a8464f94f951306082b857453275d182
xc1618 <35906469+xc1618@users.noreply.github.com>
2023-12-12 20:13:42 +0800

    del

--- 221017000258/src/scripts/test.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-classifier = pipeline("text-classification", model="waimai_10k_bert")
-while True:
-    text = input("请输入一句话（或输入q退出）：")
-    if text == "q":
-        break
-    result = classifier(text)
-    print(result)
